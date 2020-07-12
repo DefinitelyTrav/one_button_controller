17c6e80f4d6d488701ebae21bb38aff2ff47e09f
Travis Wright <21212370+DefinitelyTrav@users.noreply.github.com>
2020-07-11 23:17:29 -0400

    Update ob_controller.ino

--- ob_controller/ob_controller.ino
+++ ob_controller/ob_controller.ino
@@ -13,16 +13,18 @@ unsigned long DASH_LENGTH = 125; // Value in milliseconds. DOT_LENGTH would be a
 unsigned long RUN_TIME = 150;    // Value in milliseconds. Time after release until press is read and run.
 unsigned long HOLD_TIME = 125;
 
-int MORSE[100];
+int MORSE[10];
 int MORSE_POS = 0;
 int MORSE_MAX = 0;
-int CODE_AS_INT = 0;
+unsigned long long CODE_AS_INT = 0;
 
 unsigned long TIME, PRESS_TIME, RELEASE_TIME;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  XInput.setTriggerRange(0, 100);
 }
 
 void loop() {
@@ -66,7 +68,7 @@ void loop() {
       CODE_AS_INT = 10 * CODE_AS_INT + MORSE[i];
     }
 
-    Serial.println(CODE_AS_INT);
+    //Serial.println(CODE_AS_INT);
     RUN_MACRO(CODE_AS_INT);
 
     // Reset array.
@@ -101,14 +103,94 @@ void DEBUG_MORSE() {
   Serial.println();
 }
 
-void RUN_MACRO(int c) {
+void BUTTON_PRESS(uint8_t b, bool f) {
+  if (f){
+    XInput.setButton(b, !XInput.getButton(b));
+  } else {
+    XInput.setButton(b, true);
+    delay(100);
+    XInput.setButton(b, false);
+  }
+}
+
+void TRIGGER_PRESS(XInputControl t, int32_t v, bool b) {
+  int32_t c = XInput.getTrigger(t);
+  if (b) {
+    if (c != 0) {
+      XInput.setTrigger(t, 0);
+    } else {
+      XInput.setTrigger(t, v);
+    }
+  } else {
+    XInput.setTrigger(t, v);
+    delay(100);
+    XInput.setTrigger(t, 0);
+  }
+}
+
+void DPAD_PRESS(XInputControl p, bool b) {
+  if (b) {
+    XInput.setDpad(p, !XInput.getDpad(p));
+  } else {
+    XInput.setDpad(p, true);
+    delay(100);
+    XInput.setDpad(p, false);
+  }
+}
+
+void RUN_MACRO(unsigned long long c) {
   switch (c) {
-    case 1:
-      XInput.setButton(BUTTON_A, true);
-      delay(100);
-      XInput.setButton(BUTTON_A, false);
+    // Button presses
+    case 1:         
+      BUTTON_PRESS(BUTTON_A, false);
       break;
-    case 2:
+    case 2: 
+      BUTTON_PRESS(BUTTON_B, false);
+      break;
+    case 11: 
+      BUTTON_PRESS(BUTTON_X, false);
+      break;
+    case 22: 
+      BUTTON_PRESS(BUTTON_Y, false);
+      break;
+    case 21: 
+      BUTTON_PRESS(BUTTON_LB, false);
+      break;
+    case 12: 
+      BUTTON_PRESS(BUTTON_RB, false);
+      break;
+    case 221:
+      TRIGGER_PRESS(TRIGGER_LEFT, 100, false);
+      break;
+    case 122:
+      TRIGGER_PRESS(TRIGGER_RIGHT, 100, false);
+      break;
+    case 212:
+      BUTTON_PRESS(BUTTON_START, false);
+      break;
+    case 222:
+      BUTTON_PRESS(BUTTON_BACK, false);
+      break;
+    case 2111:
+      BUTTON_PRESS(BUTTON_L3, false);
+      break;
+    case 1112:
+      BUTTON_PRESS(BUTTON_R3, false);
+      break;
+    case 1111:
+      DPAD_PRESS(DPAD_UP, false);
+      break;
+    case 2222:
+      DPAD_PRESS(DPAD_DOWN, false);
+      break;
+    case 2121:
+      DPAD_PRESS(DPAD_LEFT, false);
+      break;
+    case 1212:
+      DPAD_PRESS(DPAD_RIGHT, false);
+      break;
+    // Quick Spin
+    case 1212121212:
       XInput.setButton(TRIGGER_LEFT, true);
       XInput.setJoystick(JOY_LEFT, true, false, false, false);
       delay(25);
@@ -128,7 +210,6 @@ void RUN_MACRO(int c) {
       XInput.setButton(BUTTON_B, true);
       delay(25);
       XInput.setJoystick(JOY_LEFT, true, false, false, false);
-      XInput.setButton(BUTTON_B, true);
       delay(100);
       XInput.setJoystick(JOY_LEFT, false, false, false, false);
       XInput.setButton(BUTTON_B, false);
